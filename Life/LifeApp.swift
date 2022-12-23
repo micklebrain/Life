d08f46bc45d731e084fc0190f6a1fc5eb375e4e1
Nathan Nguyen <micklebrain@gmail.com>
2022-12-23 13:20:54 -0500

    Initial Commit

---
+++ Life/LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeApp.swift
+//  Life
+//
+//  Created by Nathan Nguyen on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
